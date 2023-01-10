64fbf4ac383db7289dd84d0b13838046650a549f
Himanshu Bansal <himanshubansal791999@gmail.com>
2023-01-10 14:38:43 +0530

    basic setup

---
+++ app/src/main/java/com/example/cardlanandroiddevice/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.cardlanandroiddevice
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import com.example.cardlanandroiddevice.ui.theme.CardlanAndroidDeviceTheme
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CardlanAndroidDeviceTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
